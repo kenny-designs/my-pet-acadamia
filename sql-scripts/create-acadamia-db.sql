36fb7b02b0939de60bb2c3dca03677ecd7d9150f
Alexander Aguilar <alexanderman2004@yahoo.com>
2020-04-29 12:17:57 -0400

    added many combat related tables

--- sql-scripts/create-acadamia-db.sql
+++ sql-scripts/create-acadamia-db.sql
@@ -3,13 +3,21 @@ CREATE DATABASE IF NOT EXISTS pet_acadamia_db;
 USE pet_acadamia_db;
 
 -- DROP TABLES
-DROP TABLE IF EXISTS player_pets;
-DROP TABLE IF EXISTS pets;
-DROP TABLE IF EXISTS accounts;
-DROP TABLE IF EXISTS health_types;
+DROP TABLE IF EXISTS player_battle_instances;	-- ongoing pvp battles
+DROP TABLE IF EXISTS safari_battle_instances;	-- ongoing pve battles
+DROP TABLE IF EXISTS team_instances;			-- active battle pets teams
+DROP TABLE IF EXISTS battle_pets;				-- pets currently in battle
+DROP TABLE IF EXISTS player_pets;				-- pets players own
+DROP TABLE IF EXISTS safari_pets;				-- wild pets that players fight
+DROP TABLE IF EXISTS pets;						-- all pets available in the game
+DROP TABLE IF EXISTS skills;					-- skills that pets use in battle
+DROP TABLE IF EXISTS accounts;					-- player accounts
+DROP TABLE IF EXISTS health_types;				-- lookup table for hitpoint totals
+DROP TABLE IF EXISTS damages;					-- lookup table for damage types
+DROP TABLE IF EXISTS debuffs;					-- lookup table for debuff types
+DROP TABLE IF EXISTS buffs;						-- lookup table for buff types
 
 -- ACCOUNTS TABLE
-
 -- User account information
 CREATE TABLE accounts(
 	id INT AUTO_INCREMENT PRIMARY KEY,
@@ -28,7 +36,6 @@ VALUES ('kenny', 'password', 34, 4),
 UNLOCK TABLES;
 
 -- HEALTH_TYPES TABLE
-
 -- Health types and the amount of health provided at the given level
 CREATE TABLE health_types(
 	health_type VARCHAR(12) NOT NULL,
@@ -80,8 +87,17 @@ VALUES ('heavyweight', 1, 23),
 
 UNLOCK TABLES;
 
--- PETS TABLE
+-- SKILLS TABLE
+-- All skills available in the game that pets can use in battle
+CREATE TABLE skills(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(32) NOT NULL UNIQUE,
+	damage_type VARCHAR(64),
+	debuff_type VARCHAR(64),
+	buff_type VARCHAR(64)
+);
 
+-- PETS TABLE
 -- Basic pet information table
 CREATE TABLE pets(
 	id INT AUTO_INCREMENT PRIMARY KEY,
@@ -92,7 +108,11 @@ CREATE TABLE pets(
 	skill_1_id INT,
 	skill_2_id INT,
 	skill_3_id INT,
-	skill_4_id INT
+	skill_4_id INT,	
+	FOREIGN KEY (skill_1_id) REFERENCES skills(id),
+	FOREIGN KEY (skill_2_id) REFERENCES skills(id),
+	FOREIGN KEY (skill_3_id) REFERENCES skills(id),
+	FOREIGN KEY (skill_4_id) REFERENCES skills(id)
 );
 
 -- Add pet data
@@ -112,7 +132,6 @@ VALUES ('Pachino',  'heavyweight',  'pachino.png', 'A powerful bird-like pet! Pr
 UNLOCK TABLES;
 
 -- PLAYER_PETS TABLE
-
 -- Collection of all pets in the game and their owners
 CREATE TABLE player_pets(
 	id INT AUTO_INCREMENT PRIMARY KEY,	
@@ -142,4 +161,79 @@ VALUES (4, 8, 1, 1),
  
 UNLOCK TABLES;
 
+-- SAFARI_PETS TABLE
+-- Collection of all pets in the safari
+CREATE TABLE safari_pets(
+	id INT AUTO_INCREMENT PRIMARY KEY,	
+	level INT DEFAULT 1,
+	pet_id INT NOT NULL,
+	FOREIGN KEY (pet_id) REFERENCES pets(id)
+);
 
+-- BATTLE_PETS TABLE
+-- All pets that are currently fighting (both player owned and NPC)
+CREATE TABLE battle_pets(
+	id INT AUTO_INCREMENT PRIMARY KEY,	
+	hitpoints INT NOT NULL,
+	player_pet_id INT,
+	safari_pet_id INT,
+	FOREIGN KEY (player_pet_id) REFERENCES player_pets(id),
+	FOREIGN KEY (safari_pet_id) REFERENCES safari_pets(id)
+);
+
+-- DAMAGES TABLE
+-- Properties of various damage inflicting skills
+CREATE TABLE damages(
+	damage_type VARCHAR(64) NOT NULL,
+	level INT NOT NULL,
+	low_damage INT NOT NULL,
+	high_damage INT NOT NULL
+);
+
+-- DEBUFFS TABLE
+-- Properties of various debuffs that pets inflict
+CREATE TABLE debuffs(
+	debuff_type VARCHAR(64) NOT NULL,
+	level INT NOT NULL,
+	turn_duration INT NOT NULL
+);
+
+-- BUFFS TABLE
+-- Properties of various buffs pets can inflict
+CREATE TABLE buffs(
+	debuff_type VARCHAR(64) NOT NULL,
+	level INT NOT NULL,
+	turn_duration INT NOT NULL
+);
+
+-- TEAM_INSTANCES TABLE
+-- Teams of battle pets currently engaging in combat
+CREATE TABLE team_instances(
+	id INT AUTO_INCREMENT PRIMARY KEY,	
+	battle_pet_1_id INT NOT NULL,
+	battle_pet_2_id INT,
+	battle_pet_3_id INT,
+	FOREIGN KEY (battle_pet_1_id) REFERENCES battle_pets(id),
+	FOREIGN KEY (battle_pet_2_id) REFERENCES battle_pets(id),
+	FOREIGN KEY (battle_pet_3_id) REFERENCES battle_pets(id)
+);
+
+-- PLAYER_BATTLE_INSTANCES TABLE
+-- Ongoing player versus player battles
+CREATE TABLE player_battle_instances(
+	id INT AUTO_INCREMENT PRIMARY KEY,	
+	team_1_id INT NOT NULL,
+	team_2_id INT NOT NULL,
+	FOREIGN KEY (team_1_id) REFERENCES team_instances(id),
+	FOREIGN KEY (team_2_id) REFERENCES team_instances(id)
+);
+
+-- SAFARI_BATTLE_INSTANCES TABLE
+-- Ongoing player versus player battles
+CREATE TABLE safari_battle_instances(
+	id INT AUTO_INCREMENT PRIMARY KEY,	
+	player_team_id INT NOT NULL,
+	safari_team_id INT NOT NULL,
+	FOREIGN KEY (player_team_id) REFERENCES team_instances(id),
+	FOREIGN KEY (safari_team_id) REFERENCES team_instances(id)
+);
